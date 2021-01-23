217fdc3eca127a3a1911443a7f4c59762ca6693f
tejashvini10 <74137391+tejashvini10@users.noreply.github.com>
2021-01-23 15:19:22 +0530

    Update ground.js

--- ground.js
+++ ground.js
@@ -6,13 +6,13 @@ class Ground{
       }
       
       
-      this.body = Bodies.rectangle(500,630,1000,20,options);
+      this.body = Bodies.rectangle(500,900,1000,20,options);
       World.add(world, this.body);
     }
     display(){
     
       rectMode(CENTER);
       fill("brown");
-      rect(500,630,1000,20);
+      rect(500,900,1000,20);
     }
   };
