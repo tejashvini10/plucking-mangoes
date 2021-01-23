4baa0682ee41d5fe17fedfc050d82d97e43b3f96
tejashvini10 <74137391+tejashvini10@users.noreply.github.com>
2021-01-23 15:02:43 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -96,7 +96,7 @@ function detectCollision(lstone,lmango){
  mangoBodyPosition=lmango.body.position;
   stoneBodyPosition=lstone.body.position;
   var distance=dist(stoneBodyPosition.x,stoneBodyPosition.y,mangoBodyPosition.x,mangoBodyPosition.y);
-if(distance<=lmango.rlstone.r)
+if(distance<=lmango.r+lstone.r)
 {
   Matter.Body.setStatic(lmango.body,false);
 }
